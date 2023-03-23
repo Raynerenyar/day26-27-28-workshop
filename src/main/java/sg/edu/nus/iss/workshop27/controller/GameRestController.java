7e0a349b34224f6464e4b768d831f8302af27fd9
Raynerenyar <105347568+Raynerenyar@users.noreply.github.com>
2023-03-23 11:19:01 +0800

    cross origins

--- src/main/java/sg/edu/nus/iss/workshop27/controller/GameRestController.java
+++ src/main/java/sg/edu/nus/iss/workshop27/controller/GameRestController.java
@@ -14,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,6 +43,7 @@ public class GameRestController {
     private GameRepo gameRepo;
 
     @GetMapping(path = "/games", produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "#{'${client.url}'}")
     public String browseGames(
             @RequestParam(defaultValue = "25") Integer limit,
             @RequestParam(defaultValue = "0") Integer offset) {
